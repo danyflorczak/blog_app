40cb47b28912afe4d6b879412be3c8200fc78c75
Daniel Florczak <danyflorczak@gmail.com>
2023-10-20 13:16:29 +0200

    add edit to articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,20 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
@@ -31,6 +45,6 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :status)
   end
 end
